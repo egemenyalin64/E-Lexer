c427da8bd2a2f260d995bac3f6449cc578dd03a9
egemenyalin64 <egemenyalin64@gmail.com>
2026-03-29 23:04:17 +0300

    0.1.1

--- lexer.c
+++ lexer.c
@@ -9,7 +9,7 @@ Lexer_Rule_Return Lexer_Rule_int(String str) {
    Lexer_Rule_Return ret;
    ret.token.type = TOKEN_TYPE_INT;
    ret.length = value.length;
-   ((TokenData*)&ret.token.data)->int_value = value.value;
+   ARITH(TokenData, &ret.token.data, 0)->int_value = value.value;
 
    return ret;
 }
@@ -45,7 +45,55 @@ Lexer_Rule_Return Lexer_Rule_identifier(String str) {
    Lexer_Rule_Return ret;
    ret.token.type = TOKEN_TYPE_IDENTIFIER;
    ret.length = counter;
-   ((TokenData*)&ret.token.data)->str_value = (String){str.buffer - str.length + 1, counter - 1};
+   ARITH(TokenData, &ret.token.data, 0)->str_value = (String){str.buffer - str.length + 1, counter - 1};
 
    return ret;
 }
+
+void read_str(u1 *src, u1 *dest, i4 dest_size) {
+   i4 src_i = 0;
+   i4 dest_i = 0;
+   while (dest_i != dest_size) {
+      if (src_i == '\\') src_i++;
+      dest[dest_i++] = src[src_i++];
+   }
+}
+
+Lexer_Rule_Return Lexer_Rule_string(String str) {
+   if (str.length < 2 || str.buffer[0] != '"') return INV_LEXER_RULE_RET;
+
+   str.buffer += str.length;
+   i4 counter = 1 - str.length;
+   i4 escape_count = 0;
+
+   do {
+      u1 c = str.buffer[counter++];
+      if (c == '\\') counter++, escape_count++;
+      else if (c == '"') goto success;
+   } while (counter < 0);
+
+   return INV_LEXER_RULE_RET;
+
+success:
+   Lexer_Rule_Return ret;
+   ret.token.type = TOKEN_TYPE_STRING;
+   ret.length = str.length + counter;
+
+   i4 str_len = ret.length - 2 - escape_count;
+   ARITH(TokenData, &ret.token.data, 0)->str_value.length = str_len;
+
+   u1 *str_start = str.buffer - str.length + 1;
+
+   if (!escape_count) {
+      ARITH(TokenData, &ret.token.data, 0)->str_value.buffer = str_start;
+      ret.token.data[12] = 0;
+      return ret;
+   }
+
+   ret.token.data[12] = TOKEN_HEAP_ALLOCATED;
+   ARITH(TokenData, &ret.token.data, 0)->str_value.buffer = malloc(str_len);
+   read_str(str_start, ARITH(TokenData, &ret.token.data, 0)->str_value.buffer, str_len);
+
+   return ret;
+}
+
--- lexer.h
+++ lexer.h
@@ -11,6 +11,9 @@ static inline u1 is_alnum(u1 c) { return is_digit(c) || is_alnum(c); }
 #define TOKEN_TYPE_WHITESPACE 0
 #define TOKEN_TYPE_IDENTIFIER 1
 #define TOKEN_TYPE_INT 2
+#define TOKEN_TYPE_STRING 3
+
+#define TOKEN_HEAP_ALLOCATED 1
 
 typedef struct Token Token;
 typedef union TokenData TokenData;
@@ -36,5 +39,8 @@ struct Lexer_Rule_Return {
 PREFIX Lexer_Rule_Func Lexer_Rule_int;
 PREFIX Lexer_Rule_Func Lexer_Rule_whitespace;
 PREFIX Lexer_Rule_Func Lexer_Rule_identifier;
+PREFIX Lexer_Rule_Func Lexer_Rule_string;
+
+void read_str(u1 *src, u1 *dest, i4 dest_size);
 
 #endif
